8da2d7717b64277ecf80fc9a34b910531405c93e
Brandon Fulljames <bfullj@gmail.com>
2024-01-12 08:02:51 +0900

    Fix font size format

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "${fontName}:pixelsize=${fontSize}:antialias=true:autohint=true";
+static char *font = "${fontName}:pixelsize=${toString fontSize}:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
